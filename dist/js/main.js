d576e1baab61858d41c8919e5d40e07f046394d7
infesto <avspyros@gmail.com>
2021-09-06 17:11:33 +0300

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,44 @@
+// Select DOM elements
+const glossaryIndex = document.querySelectorAll('.larger')
+const modal = document.querySelector("#my-modal")
+const closeBtn = document.querySelector(".close")
+
+
+// Events
+glossaryIndex.forEach(function (glossaryEntry) {
+
+  glossaryEntry.addEventListener('click', openModal)
+  closeBtn.addEventListener("click", closeModal)
+  window.addEventListener("click", outsideClick)
+
+  glossaryEntry.addEventListener('click', function (event) {
+    console.log(event.target.id)
+  })
+
+})
+
+
+
+
+
+
+// Open
+function openModal() {
+  modal.style.display = "block"
+}
+
+// Close
+function closeModal() {
+  modal.style.display = "none"
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = "none"
+  }
+}
+
+
+
+
